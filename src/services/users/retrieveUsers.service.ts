29890ff8f954a896dbc0c2d95fb8d21e41f544f6
Igorttdp <igorttdp.dev@email.com>
2023-04-08 14:51:02 -0300

    feat: add retrieveUsersServices
    This service search all users in db and return the serialized data

---
+++ src/services/users/retrieveUsers.service.ts
@@ -0,0 +1,10 @@
+import { prisma } from "../../app";
+import { retrieveUsersSchema } from "../../schemas/user.schemas";
+
+const retrieveUsersService = async () => {
+  const users = await prisma.user.findMany();
+  
+  return retrieveUsersSchema.parse(users)
+};
+
+export default retrieveUsersService;
