570000f6d229fc20fe42122413d5e8147b20f4dd
Igorttdp <igorttdp.dev@email.com>
2023-04-08 14:36:23 -0300

    feat: add users CRUD routes

--- src/routers/users.routes.ts
+++ src/routers/users.routes.ts
@@ -1,5 +1,34 @@
 import { Router } from "express";
+import {
+  createUserController,
+  retrieveUsersController,
+  updateUserController,
+  deleteUserController,
+} from "../controllers/users";
+import {
+  ensureUserIsValidMiddleware,
+  validateSchemasMiddleware,
+  validateTokenMiddleware,
+} from "../middlewares";
+import createUserRequestSchema, {
+  updateUserRequestSchama,
+} from "../schemas/user.schemas";
 
 const usersRoutes = Router();
+
+usersRoutes.get("", retrieveUsersController);
+usersRoutes.post(
+  "",
+  validateSchemasMiddleware(createUserRequestSchema),
+  ensureUserIsValidMiddleware,
+  createUserController
+);
+usersRoutes.patch(
+  "/profile",
+  validateTokenMiddleware,
+  validateSchemasMiddleware(updateUserRequestSchama),
+  updateUserController
+);
+usersRoutes.delete("/profile", validateTokenMiddleware, deleteUserController);
 
 export default usersRoutes;
