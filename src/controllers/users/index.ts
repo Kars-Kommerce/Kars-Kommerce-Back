2215837c315c4eb97d7838831890bba1a14888d2
Igorttdp <igorttdp.dev@email.com>
2023-04-08 15:08:30 -0300

    centralize imports

---
+++ src/controllers/users/index.ts
@@ -0,0 +1,6 @@
+import createUserController from "./createUser.controller";
+import retrieveUsersController from "./retrieveUsers.controller";
+import updateUserController from "./updateUser.controller";
+import deleteUserController from "./deleteUser.controller";
+
+export { createUserController, retrieveUsersController, updateUserController, deleteUserController };
