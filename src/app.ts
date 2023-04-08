0d6f926c1a0c56777e20ce3bf1e7b09a1a7e15d4
Igorttdp <igorttdp.dev@email.com>
2023-04-08 14:34:28 -0300

    feat: add errorHandler, express-async-errors, login route and dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import "reflect-metadata";
 import express, { json } from "express";
+import "express-async-errors";
 import { PrismaClient } from "@prisma/client";
 import usersRoutes from "./routers/users.routes";
+import loginRoute from "./routers/login.route";
+import "dotenv/config";
+import { errorHandler } from "./errors";
 
 export const prisma: PrismaClient = new PrismaClient();
+
 const app = express();
 app.use(json());
 
 app.use("/users", usersRoutes);
+app.use("/login", loginRoute);
+
+app.use(errorHandler);
 
 export default app;
