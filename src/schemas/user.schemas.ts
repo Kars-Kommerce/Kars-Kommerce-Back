7e85bf6ad504da15cc9d7d3d8882817af2a9b745
Igorttdp <igorttdp.dev@email.com>
2023-04-08 14:55:12 -0300

    feat: Add all zod schemas to user CRUD routes

---
+++ src/schemas/user.schemas.ts
@@ -0,0 +1,55 @@
+import { z } from "zod";
+import {
+  IUserRequestProps,
+  IUserResponseProps,
+} from "../interfaces/user.interfaces";
+import moment from "moment";
+
+const createUserRequestSchema = z.object({
+  name: z.string().max(50, "Deve conter no máximo 50 caracteres"),
+  username: z.string().max(30, "Deve conter no máximo 30 caracteres"),
+  email: z.string().email("Deve ser um email válido"),
+  password: z.string().max(120, "Máximo de 120 caracteres"),
+  cpf: z.string().regex(/^\d{11}$/, "Deve conter 11 números"),
+  cellphone: z
+    .string()
+    .min(10)
+    .max(11)
+    .regex(/^\d{10,11}$/, "Deve conter entre 10 a 11 números"),
+  birth_date: z
+    .string()
+    .regex(
+      /^(0[1-9]|[12]\d|3[01])([-\/])(0[1-9]|1[0-2])\2(\d{4})$/g,
+      "Insira uma data válida no formato DD-MM-YYYY"
+    )
+    .transform((val) => moment(val, "DD-MM-YYYY").toISOString())
+    .refine((val) => moment(val).isValid(), "Insira uma data válida"),
+  bio: z.string().max(500).optional(),
+  is_advertiser: z.boolean().optional(),
+});
+
+export const createListUserResponseSchema = z.object({
+  id: z.string().uuid(),
+  name: z.string(),
+  username: z.string(),
+  email: z.string().email(),
+  cpf: z.string(),
+  cellphone: z.string(),
+  birth_date: z.date(),
+  bio: z.string().optional(),
+  is_advertiser: z.boolean(),
+  created_at: z.date(),
+  updated_at: z.date(),
+});
+
+export const retrieveUsersSchema: z.ZodSchema<IUserResponseProps[]> = z.array(
+  createListUserResponseSchema
+);
+
+export const updateUserRequestSchama = createUserRequestSchema
+  .omit({
+    cpf: true,
+  })
+  .partial();
+
+export default createUserRequestSchema;
