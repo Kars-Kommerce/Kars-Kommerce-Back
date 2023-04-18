162fbbdb0a9dd461dfc74f4e63de3d37fb475a00
IgorD934 <igord934@gmail.com>
2023-04-18 16:17:50 -0300

    Create paginations of ads

--- src/controllers/ads/listAds.controller.ts
+++ src/controllers/ads/listAds.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express-serve-static-core";
 import listAdsService from "../../services/ads/listAds.service";
 
-
 const listAdsController = async (req: Request, res: Response) => {
+  const page = Number(req.query.page) || 1;
 
-  const ads = await listAdsService();
+  const ads = await listAdsService(page, 12);
 
   return res.status(200).json(ads);
 };
--- src/services/ads/listAds.service.ts
+++ src/services/ads/listAds.service.ts
@@ -1,10 +1,26 @@
 import { prisma } from "../../app";
 
-const listAdsService = async () => {
-  const ads = await prisma.advertisement.findMany()
+const listAdsService = async (page = 1, pageSize = 12) => {
+  const offset = (page - 1) * pageSize;
 
-  
-  return ads
+  const ads = await prisma.advertisement.findMany({
+    skip: offset,
+    take: pageSize,
+    orderBy: {
+      created_at: "asc",
+    },
+  });
+
+  const totalAds = await prisma.advertisement.count();
+
+  const pageCount = Math.ceil(totalAds / pageSize);
+
+  return {
+    data: ads,
+    page,
+    pageCount,
+    total: totalAds,
+  };
 };
 
 export default listAdsService;
