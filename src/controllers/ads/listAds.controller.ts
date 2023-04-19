2e0200bf8d590009611dafb8e0d804addce18646
IgorD934 <igord934@gmail.com>
2023-04-19 14:42:22 -0300

    Add pageSize params

--- src/controllers/ads/listAds.controller.ts
+++ src/controllers/ads/listAds.controller.ts
@@ -3,8 +3,9 @@ import listAdsService from "../../services/ads/listAds.service";
 
 const listAdsController = async (req: Request, res: Response) => {
   const page = Number(req.query.page) || 1;
+  const pageSize = Number(req.query.pageSize) || 12;
 
-  const ads = await listAdsService(page, 12);
+  const ads = await listAdsService(page, pageSize);
 
   return res.status(200).json(ads);
 };
